d7892ff86c68005bd005fcdb4113c3efadb72072
seungju <kingsky32@gmail.com>
2020-08-07 03:26:19 +0900

    Getting Main Slide

--- src/components/MainSlide.js
+++ src/components/MainSlide.js
@@ -1,7 +1,78 @@
 import React from "react";
+import YouTube from "react-youtube";
+import styled from "styled-components";
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y, EffectCoverflow } from "swiper";
 
-function MainSlide(props) {
-  return <div />;
-}
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss";
+import "swiper/components/scrollbar/scrollbar.scss";
+
+const Container = styled.div`
+  height: 30rem;
+  .swiper-button-prev,
+  .swiper-button-next {
+    color: ${props => props.theme.whiteColor};
+    width: 3.4rem;
+    height: 3.4rem;
+    border-radius: .5rem;
+    &::after {
+      font-size: 1.5rem;
+    }
+    &:hover {
+      background-color: ${props => props.theme.whiteColor}25;
+    }
+  }
+`;
+
+const Slide = styled.div`
+  height: 30rem;
+  background-color: ${props => props.theme.darkColor};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+// install Swiper components
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, EffectCoverflow]);
+
+const MainSlide = ({ slide = [] }) => {
+  const opts = {
+    width: "520",
+    height: "300",
+    playerVars: {
+      autoplay: 0
+    }
+  };
+  return (
+    <Container>
+      <Swiper
+        effect="coverflow"
+        slidesPerView="3"
+        spaceBetween={50}
+        centeredSlides={true}
+        loop={true}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 150,
+          modifier: 1,
+          slideShadows: true
+        }}
+        navigation
+      >
+        {slide.map(slide =>
+          <SwiperSlide>
+            <Slide>
+              <YouTube videoId={slide.videoId} opts={opts} />
+            </Slide>
+          </SwiperSlide>
+        )}
+      </Swiper>
+    </Container>
+  );
+};
 
 export default MainSlide;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import DailyBoxOffice from "../components/DailyBoxOffice";
 import WeeklyBoxOffice from "../components/WeeklyBoxOffice";
+import MainSlide from "../components/MainSlide";
 
 const Container = styled.div`
   flex: 1;
@@ -11,6 +12,15 @@ const Container = styled.div`
 const Movies = () => {
   return (
     <Container>
+      <MainSlide
+        slide={[
+          { videoId: "919GruRSZzE" },
+          { videoId: "fcueArgm800" },
+          { videoId: "KKPUEANoHAo" },
+          { videoId: "KrM3vS5sy2w" },
+          { videoId: "n7LZA4Er7_g" }
+        ]}
+      />
       <DailyBoxOffice />
       <WeeklyBoxOffice />
     </Container>
