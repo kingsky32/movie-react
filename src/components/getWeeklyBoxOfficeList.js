c69a84ec06fe46b523c77958f4fab8944589f059
seungju <kingsky32@gmail.com>
2020-08-08 04:51:56 +0900

    Update getWeeklyBoxOfficeList date time

--- src/components/getWeeklyBoxOfficeList.js
+++ src/components/getWeeklyBoxOfficeList.js
@@ -3,7 +3,7 @@ import moment from "moment";
 import { KOBIS_API_KEY, KOBIS_END_POINT } from "../config";
 
 const getMovie = async ({ targetDate }) => {
-  const targetDt = moment(targetDate).subtract(5, "days").format("YYYYMMDD");
+  const targetDt = moment(targetDate).subtract(7, "days").format("YYYYMMDD");
   try {
     const { data: { boxOfficeResult } } = await Axios.get(
       `${KOBIS_END_POINT}searchWeeklyBoxOfficeList.json?key=${KOBIS_API_KEY}&targetDt=${targetDt}`
